1da48791dcb4c8e30d80a912c8473e489cff8616
Thomas Maestas <thomasm1.maestas@gmail.com>
2022-10-29 22:12:08 -0600

    cli dispatcher

---
+++ cli/src/main/java/xyz/cryptomaven/app/controllers/ViewUserCommand.java
@@ -0,0 +1,19 @@
+package xyz.cryptomaven.app.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import xyz.cryptomaven.app.models.User;
+
+public class ViewUserCommand implements Command {
+ 
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		User user = new User(1, "tom", "m");
+		request.setAttribute("userDetails", user);
+		return "User details here";
+	}
+ 
+
+}

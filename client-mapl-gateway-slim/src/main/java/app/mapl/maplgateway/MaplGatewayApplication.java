9bf287da20ccd5aeb6d8cef9d90735825353d957
Thomas Maestas <thomas1.maestas@gmail.com>
2024-08-28 21:45:08 -0400

    gateway reactive filters

---
+++ client-mapl-gateway-slim/src/main/java/app/mapl/maplgateway/MaplGatewayApplication.java
@@ -0,0 +1,58 @@
+package app.mapl.maplgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaplGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaplGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator eazyBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/demobank/accounts/**")
+						.filters( f -> f.rewritePath("/demobank/accounts/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.circuitBreaker(config -> config.setName("accountsCircuitBreaker")
+										.setFallbackUri("forward:/contactSupport")))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/demobank/loans/**")
+						.filters( f -> f.rewritePath("/demobank/loans/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.retry(retryConfig -> retryConfig.setRetries(3)
+										.setMethods(HttpMethod.GET)
+										.setBackoff(Duration.ofMillis(100),Duration.ofMillis(1000),2,true)))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/demobank/cards/**")
+						.filters( f -> f.rewritePath("/demobank/cards/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+										.setKeyResolver(userKeyResolver())))
+						.uri("lb://CARDS")).build();
+	}
+
+	@Bean
+	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+				.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
+	}
+
+	@Bean
+	public RedisRateLimiter redisRateLimiter() {
+		return new RedisRateLimiter(1, 1, 1);
+	}
+
+	@Bean
+	KeyResolver userKeyResolver() {
+		return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user"))
+				.defaultIfEmpty("anonymous");
+	}
+
+}
---
+++ client-mapl-gateway-slim/src/test/java/app/mapl/maplgateway/MaplGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package app.mapl.maplgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaplGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

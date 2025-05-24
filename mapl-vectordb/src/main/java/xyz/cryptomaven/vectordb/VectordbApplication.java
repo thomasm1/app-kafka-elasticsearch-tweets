8fc4032e239d7288679f33f5279a822771406847
Thomas Maestas <thomas1.maestas@gmail.com>
2025-05-23 22:41:53 -0400

    mapl vectordb start

---
+++ mapl-vectordb/src/main/java/xyz/cryptomaven/vectordb/VectordbApplication.java
@@ -0,0 +1,13 @@
+package xyz.cryptomaven.vectordb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VectordbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VectordbApplication.class, args);
+	}
+
+}
---
+++ mapl-vectordb/src/test/java/xyz/cryptomaven/vectordb/VectordbApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.cryptomaven.vectordb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VectordbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
